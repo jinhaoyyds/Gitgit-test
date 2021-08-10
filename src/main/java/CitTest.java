b4aa7ba337337d057d5f2f00b5cd7d6f5dcddba7
jinhao <jh159309@.com>
2021-08-10 15:07:59 +0800

    hot-fix test

--- src/main/java/CitTest.java
+++ src/main/java/CitTest.java
@@ -6,5 +6,6 @@ public class CitTest {
     public static void main(String[] args) {
         System.out.println("我是版本1.0.0");
         System.out.println("我是版本1.0.1");
+        System.out.println("hot-fix  test");
     }
 }
