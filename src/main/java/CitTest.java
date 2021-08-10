db315d7524e921dc2c5737d9467e48839e8a0ee6
jinhao <jh159309@.com>
2021-08-10 15:05:39 +0800

    版本1.0.1，初始化项目

---
+++ src/main/java/CitTest.java
@@ -0,0 +1,9 @@
+/**
+ * @Author: jinhao
+ * @DateTime: 2021/8/10 --15:04
+ */
+public class CitTest {
+    public static void main(String[] args) {
+        System.out.println("我是版本1.0.0");
+    }
+}
